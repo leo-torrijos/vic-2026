4e58b84b6b0467cfa8130dc0f016f0bfc38d73bb
Leo Torrijos <holdstill04@gmail.com>
2026-03-27 17:02:54 -0400

    Good player, interaction, messes
    * Player now has mouse look and proper input
    * Basic interaction system (interaction trigger)
    * Player "cleaning" start
    * Test mess, cannot be cleaned yet
    * 3D physics layers

--- gameobjects/player/Player.gd
+++ gameobjects/player/Player.gd
@@ -1,28 +1,63 @@
 extends CharacterBody3D
 
+enum {FREEZE, MOVE, CLEAN, DIE}
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 
+const LOOK_SENSITIVITY = 0.005
+const ACCEL = 1.0
+
+var state = MOVE
+
+@onready var neck = $Neck
+@onready var camera = $Neck/Camera3D
+
+
+func _ready() -> void:
+	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	match state:
+		FREEZE:
+			velocity = Vector3.ZERO
+		MOVE:
+			# Add the gravity.
+			if not is_on_floor():
+				velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+			var input_dir = Input.get_vector("left", "right", "forward", "backward")
+			var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+			if direction:
+					#$CameraAnim.play("run")
+				#else:
+					#$CameraAnim.play("walk")
+				velocity.x = lerp(velocity.x, direction.x * SPEED, ACCEL)
+				velocity.z = lerp(velocity.z, direction.z * SPEED, ACCEL)
+				
+			else:
+				velocity.x = move_toward(velocity.x, 0.0, ACCEL)
+				velocity.z = move_toward(velocity.z, 0.0, ACCEL)
+			
+			# Action (i.e. using item, cleaning)
+			if Input.is_action_just_pressed("action1"):
+				var interaction = $Neck/Camera3D/InteractRay.get_collider()
+				if interaction and interaction is InteractTrigger:
+					match interaction.interaction_type:
+						"clean":
+							state = CLEAN
+					print_debug("I INTERACTED")
+		CLEAN:
+			velocity = Vector3.ZERO
+			if Input.is_action_just_released("action1"):
+				state = MOVE
 	move_and_slide()
+	
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion and state != CLEAN:
+			neck.rotate_y(-event.relative.x * LOOK_SENSITIVITY)
+			camera.rotate_x(-event.relative.y * LOOK_SENSITIVITY)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
---
+++ gameobjects/triggers/InteractTrigger.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+class_name InteractTrigger
+
+## What type of interaction this is to query crosshair changes. Example: "clean" will show a cleaning icon. See script for valid inputs.
+# VALID INPUTS:
+# inspect
+# clean
+@export var interaction_type = ""
