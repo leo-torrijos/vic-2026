72ffebb690a6c51d69bf6142ddef1589df91aeff
Leo Torrijos <holdstill04@gmail.com>
2026-03-28 14:46:09 -0400

    level director scaffholding again

--- levels/LevelDirector.gd
+++ levels/LevelDirector.gd
@@ -4,38 +4,47 @@ extends Node3D
 ## State Enum
 enum {
 	INTRO,  ## Opening cutscene
-	PLANNING,  ## Planning phase; ends with murder
+	CALM,  ## Planning phase; ends with murder
+	MURDER,
 	CLEANUP,  ## Cleanup phase; ends with cops' arrival
 	INVESTIGATION,  ## Investigation phase; ends with cops finishing their route
 	WIN,  ## Outro cutscene (if you win)
-	LOSS  ## Outro cutscene (if you lose)
+	LOSE  ## Outro cutscene (if you lose)
 }
+
+@export var cleanup_time = 20.0
 
 var phase = 0
 
 const SUSPICION_LIMIT = 100
-var state = INTRO
+#var state = INTRO
 var suspicion = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$CleanupTimer.wait_time = cleanup_time
 
 
 func next_phase():
 	phase += 1
 	# TODO: could use enum with these
 	match phase:
-		1: # Calm (Scope Out)
+		INTRO:
 			pass
-		2: # Murder
+		CALM: # Calm (Scope Out)
 			pass
-		3: # Cleanup
+		MURDER: # Murder
+			print_debug("YOU KILLED THEM...")
 			pass
-		4: # Investigation
+		CLEANUP: # Cleanup
+			print_debug("Cleanup!")
 			pass
-		5: # Win
+		INVESTIGATION: # Investigation
+			print_debug("Investigation begins. POLICE INCOMING!")
+		WIN: # Win
+			pass
+		LOSE: # Lose :(
 			pass
 
 #func _physics_process(_delta: float) -> void:
@@ -45,10 +54,15 @@ func next_phase():
 
 ## Intro -> Planning: Cutscene stuff
 ## Planning -> Cleanup: Victim Actor emits signal on death
+# TODO: connect this to victim dying (current only one)
+# NOTE: Make sure if multiple victims are added this is improved
+func _on_victim_die():
+	next_phase()
+	$CleanupTimer.start()
 
 ## Cleanup -> Investigation: Timer runs out
 func _on_cleanup_timer_timeout() -> void:
-	state = INVESTIGATION
+	next_phase()
 	# TODO: spawn cops, set routes
 
 ## Investigation -> Win: All cops finish routes
