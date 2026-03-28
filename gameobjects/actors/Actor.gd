852c9e856a6ea6b0a885c3e082c2303079ed5a9f
Leo Torrijos <holdstill04@gmail.com>
2026-03-28 00:15:20 -0400

    actor navigation
    * Actors can now follow walk paths

--- gameobjects/actors/Actor.gd
+++ gameobjects/actors/Actor.gd
@@ -1,28 +1,63 @@
 extends CharacterBody3D
 
+enum {IDLE, PATROL}
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var move_speed = 0.75
 
+# State
+@export var health = 2.0
+var state = IDLE
+## Removed with damage. Humans with protection cannot be infected.
+@export var protection = 0
+## If immune, this human can NEVER be infected.
+@export var immune = false
+## Things this human can interact with
+@export var unlockable_objects : Array[Node3D]
+
+# Navigation
+@onready var nav_agent = $NavigationAgent3D
+const navigation_cycle = 10
+var current_navigation_delay = 0
+var next_position
+var follow_director
+var level
+
+signal selected(human)
+signal die
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	match state:
+		IDLE:
+			velocity = Vector3.ZERO
+		PATROL:
+			move()
+			move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+func move():
+	current_navigation_delay += 1
+	if not next_position:
+		current_navigation_delay = navigation_cycle
+	if current_navigation_delay == navigation_cycle:
+		next_position = nav_agent.get_next_path_position()
+		current_navigation_delay = 0
+	velocity = lerp(velocity, (next_position - global_transform.origin).normalized() * move_speed, 0.05)
+	velocity.y = 0
+	if next_position != global_position:
+		look_at(next_position)
+		#if move_speed > WALK_SPEED:
+			#anim.play("run")
+		#else:
+			#anim.play("walk")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		#anim.play("idle")
+		pass
+	rotation.x = 0
+	rotation.z = 0
 
-	move_and_slide()
+func patrol(target_position):
+	state = PATROL
+	nav_agent.set_target_position(target_position)
+
+
+func _on_navigation_agent_3d_target_reached() -> void:
+	follow_director._on_target_reached()
---
+++ gameobjects/actors/navigation/WalkPath.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+### Path thingy for Actors to walk along.
+
+@export var new_pos_time = 0.1
+@export var navigator : Node3D
+# TODO: actually implement "finishing" paths
+## Whether this path should be looped or only be travelled once
+@export var loop_path = true
+
+var current_index = -1
+
+# Add Node3Ds as children
+var follow_points = []
+var current_follow_goal : Node3D
+
+func _ready():
+	for i in get_children():
+		if i is Node3D:
+			follow_points.append(i)
+	assert(follow_points.size() > 0)
+	navigator.connect("target_reached", _on_target_reached)
+	navigator.follow_director = self
+	_on_target_reached()
+	hide()
+
+func _on_target_reached():
+	if new_pos_time > 0.0:
+		await get_tree().create_timer(new_pos_time).timeout
+	current_index += 1
+	if current_index >= follow_points.size():
+		current_index = 0
+	current_follow_goal = follow_points[current_index]
+	#print("LET'S GO")
+	#print(current_follow_goal.global_position)
+	if navigator:
+		navigator.patrol(current_follow_goal.global_position)
