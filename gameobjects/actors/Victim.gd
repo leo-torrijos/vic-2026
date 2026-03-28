c0b9bd2ab0b3ab430740c7edd70c88addaab7ae5
Leo Torrijos <holdstill04@gmail.com>
2026-03-28 15:34:18 -0400

    victims leave corpses when dead

--- gameobjects/actors/Victim.gd
+++ gameobjects/actors/Victim.gd
@@ -1,8 +1,15 @@
 extends Actor
 class_name Victim
 
+var corpse_scene = preload("res://gameobjects/draggable/Corpse.tscn")
+
 signal death
 
 func die():
 	death.emit()
-	print("DEAD")
+	
+	var corpse = corpse_scene.instantiate()
+	corpse.global_transform = global_transform
+	get_parent().add_child(corpse)
+	print_debug("DEAD")
+	queue_free()
