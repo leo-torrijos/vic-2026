dc3cab3eb392f172a8676810c918a03ad36147ca
Leo Torrijos <holdstill04@gmail.com>
2026-03-29 00:54:26 -0400

    global my sweet summer child

--- Global.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var total_suspicion = 0.0
-var current_level_director
-
-func add_suspicion(amount):
-	total_suspicion += amount
-	current_level_director.update_suspicion_ui(total_suspicion)
