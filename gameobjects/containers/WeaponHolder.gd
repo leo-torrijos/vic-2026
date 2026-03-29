ec2e65ec3ff00b9e9967880805e2d2aaf7a8d4cb
Leo Torrijos <holdstill04@gmail.com>
2026-03-29 17:13:09 -0400

    weapon holder suspicion

--- gameobjects/containers/WeaponHolder.gd
+++ gameobjects/containers/WeaponHolder.gd
@@ -12,9 +12,10 @@ func place_object(pickup: Pickup):
 	held_object.global_position = $StoragePoint.global_position
 	held_object.global_rotation = $StoragePoint.global_rotation
 	$InteractTrigger.interaction_type = "take_from_holder"
-
+	$SuspicionArea/CollisionShape3D.set_deferred("disabled", false)
 
 func yield_object():
 	$InteractTrigger.interaction_type = "put_in_holder"
 	$StoragePoint.remove_child(held_object)
+	$SuspicionArea/CollisionShape3D.set_deferred("disabled", true)
 	return held_object.collected_object.instantiate()
